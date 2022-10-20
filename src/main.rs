624603be10002fc85bd43e2d8fdf4aea6b4efcef
Alexandru Hutanu <116074307+ahutanu@users.noreply.github.com>
2022-10-20 12:03:09 +0300

    Update main.rs
    changed int types

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::io;
 
 fn main() {
-    let mut f_n_minus_1: i128 = 0; //first
-    let mut f_n_minus_2: i128 = 1; //second
+    let mut f_n_minus_1: u128 = 0; //first
+    let mut f_n_minus_2: u128 = 1; //second
 
-    let sequence_run_times: i32 = loop {
+    let sequence_run_times: u32 = loop {
         println!("Please enter up until which Fibonacci number you'd like the sequence to run.");
 
         let mut sequence_run_times: String = String::new();
@@ -19,11 +19,11 @@ fn main() {
         };
     };
 
-    let mut iterations: i32 = 0;
+    let mut iterations: u32 = 0;
     while iterations < sequence_run_times {
         println!("Next numbers in the sequence are: {f_n_minus_1} & {f_n_minus_2}.");
 
-        let current_number: i128 = f_n_minus_1 + f_n_minus_2;
+        let current_number: u128 = f_n_minus_1 + f_n_minus_2;
 
         println!("Fibonacci number {} is: {f_n_minus_2}.", iterations+1);
 
